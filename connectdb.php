782d47f9a0292f3ba99d7d69ae704dec836f3161
Giang 112000 <giangnt.b18dcpt065.ptit@gmail.com>
2022-04-07 20:10:08 +0700

    update

--- connectdb.php
+++ connectdb.php
@@ -1,9 +1,10 @@
 <?php
-        $dbhost ="localhost";
-        $dbuser="root";
-        $dbpass="";
-        $dbname="ptitcovid";
-        $connectdb = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+        $dbhost ="x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+        $dbuser="ywc50ze2or1zqfzz";
+        $dbpass="akfayqcs13esmqp0";
+        $dbname="rihsfukiozhtkti7";
+        $port="3306";
+        $connectdb = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname,$port);
         if($connectdb){
                 mysqli_query($connectdb,"SET NAMES 'utf8'");
         } else {
