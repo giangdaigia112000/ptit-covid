ba9e939132da88649d6a879bcb814c6a35402e9a
Giang Nguyen <67044690+giangdaigia112000@users.noreply.github.com>
2023-02-13 17:52:33 +0700

    update db cof

--- connectdb.php
+++ connectdb.php
@@ -1,8 +1,8 @@
 <?php
-        $dbhost ="x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-        $dbuser="ywc50ze2or1zqfzz";
-        $dbpass="akfayqcs13esmqp0";
-        $dbname="rihsfukiozhtkti7";
+        $dbhost ="o2olb7w3xv09alub.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+        $dbuser="t2td0zkieyi5c6lw";
+        $dbpass="oij9hp5tiff12h95";
+        $dbname="svw1fm9rmdyc1cfy";
         $port="3306";
         $connectdb = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname,$port);
         if($connectdb){
